f00ee4993fd84ed5132d0d6d205e93220ed86113
GuiBret <guillaume.bretzner@gmail.com>
2020-09-07 01:57:39 +0200

    Handle themes + add controls

--- src/app/pages/todo-list/todo-list.component.ts
+++ src/app/pages/todo-list/todo-list.component.ts
@@ -3,6 +3,7 @@ import { Title } from '@angular/platform-browser';
 import { HttpService } from 'src/app/services/http.service';
 import { Subscription } from 'rxjs';
 import { TodoListService } from 'src/app/services/todo-list.service';
+import { Todo } from 'src/app/models/todo';
 
 @Component({
   selector: 'app-todo-list',
@@ -11,19 +12,21 @@ import { TodoListService } from 'src/app/services/todo-list.service';
 })
 export class TodoListComponent implements OnInit, OnDestroy {
 
+
   todoListChanged$: Subscription;
-  todos: Array<any> = [];
-  themes: Array<any>;
-  displayedColumns = ['theme', 'content', 'controls'];
+  todos: Array<Todo> = [];
+  themes: Array<string> = [];
+  displayedColumns = ['theme', 'title', 'content', 'controls'];
   constructor(private titleService: Title, private todoSvc: TodoListService) { }
 
   ngOnInit(): void {
 
     this.titleService.setTitle('TodoList - List');
 
-    this.todoListChanged$ = this.todoSvc.fetchTodosAndThemes().subscribe((response) => {
-      console.log(response);
-      this.todos = response;
+    this.todoListChanged$ = this.todoSvc.fetchTodosAndThemes().subscribe((response: {todos: Array<Todo>, themes: Array<string> }) => {
+
+      this.todos = response.todos;
+      this.themes = response.themes;
     });
   }
 
