68d762a635792a9872afddfbfeb174fdcf9f62e3
GuiBret <guillaume.bretzner@gmail.com>
2020-09-07 01:57:14 +0200

    Edit todo data (add themes)

--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -7,26 +7,44 @@ import { Todo } from '../models/todo';
   providedIn: 'root'
 })
 export class TodoListService {
+  todos: Array<Todo> = [
+    {
+      _id: "Dummy1",
+      title: "My title 1",
+      theme: "Thème 1",
+      content: "Contenu 1",
+    },
+    {
+      _id: "Dummy2",
+      title: 'My title 2',
+      theme: "Thème 2",
+      content: "Contenu 2",
+    },
 
+  ];
+  themes: Array<string> = ['Dummy 1', 'Dummy 2'];
   constructor(private http: HttpService) { }
 
-  fetchTodosAndThemes(): Observable<Array<Todo>> {
+  fetchTodosAndThemes(): Observable<{todos: Array<Todo>, themes: Array<string>}> {
     return new Observable((observer) => {
-      const dummyData = [
-      {
-        _id: "Dummy 1",
-        theme: "Thème 1",
-        content: "Contenu 1",
-      },
-      {
-        _id: "Dummy 2",
-        theme: "Thème 2",
-        content: "Contenu 2",
-      },
+      const dummyData = {
+        todos: [...this.todos],
+        themes: [...this.themes]
 
-    ]
+
+      };
 
       observer.next(dummyData);
-    })
+    });
+  }
+
+  getTodo(id: string) {
+    return this.todos.find((elem) => {
+
+      return elem._id === id;
+
+    });
+
+
   }
 }
