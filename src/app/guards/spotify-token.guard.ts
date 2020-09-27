1a488d9b682ba93fecbe8d78fbf27ceab3269a48
GuiBret <guillaume.bretzner@gmail.com>
2020-09-27 02:50:03 +0200

    Remove dependency

--- src/app/guards/spotify-token.guard.ts
+++ src/app/guards/spotify-token.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import jwt_decode from 'jwt-decode';
+
 
 @Injectable({
   providedIn: 'root'
