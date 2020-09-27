0edcc72dfdfa1e443488924faa7231051dac7dbb
GuiBret <guillaume.bretzner@gmail.com>
2020-09-27 04:07:30 +0200

    Add title

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home-page',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleSvc: Title) {
+    this.titleSvc.setTitle('Dashboard - Home Page')
+  }
 
   ngOnInit(): void {
   }
