fbf5339db6e9d8e85645adbdfee820aa46790882
GuiBret <guillaume.bretzner@gmail.com>
2020-11-11 00:03:59 +0100

    Spec : Add test

--- src/app/services/Spotify/spotify.service.spec.ts
+++ src/app/services/Spotify/spotify.service.spec.ts
@@ -1,11 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
 
 import { SpotifyService } from './spotify.service';
 
 describe('SpotifyService', () => {
   let service: SpotifyService;
   let httpClientStub: Partial<HttpClient>;
+
+  httpClientStub = {
+    get: jasmine.createSpy().and.returnValue(new Observable())
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -19,4 +24,6 @@ describe('SpotifyService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+
 });
