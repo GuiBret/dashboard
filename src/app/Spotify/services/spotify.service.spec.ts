9df5ca8550b0aa5f589e6ef8cf197d42ccc92c4d
GuiBret <guillaume.bretzner@gmail.com>
2020-11-22 21:43:44 +0100

    Test : Add test

--- src/app/Spotify/services/spotify.service.spec.ts
+++ src/app/Spotify/services/spotify.service.spec.ts
@@ -9,9 +9,11 @@ describe('SpotifyService', () => {
   let service: SpotifyService;
   let httpClientStub: Partial<HttpClient>;
 
+  const putSpyObj = jasmine.createSpy().and.returnValue(new Observable());
   const getSpyObj = jasmine.createSpy().and.returnValue(new Observable());
   httpClientStub = {
-    get: getSpyObj
+    get: getSpyObj,
+    put: putSpyObj
   };
 
   beforeEach(() => {
@@ -48,8 +50,22 @@ describe('SpotifyService', () => {
       service.fetchAutocomplete('test', {albums: true, songs: true, artists: true});
 
       expect(httpClientStub.get).toHaveBeenCalledWith(environment.serverRoot + '/spotify/autocomp/test?type=album,artist,track');
-    })
-  })
+    });
+  });
+
+  describe('Play element', () => {
+    it('should have made PUT request with an attribute "uri" since we are trying to play a song', () => {
+      putSpyObj.calls.reset();
+      service.playElement('randomuri', 'track');
+      expect(httpClientStub.put).toHaveBeenCalledWith('https://api.spotify.com/v1/me/player/play', {uris: ['randomuri']});
+    });
+
+    it('should have made PUT request with an attribute "context_uri" since we are trying to play an album (same for an album)', () => {
+      putSpyObj.calls.reset();
+      service.playElement('randomuri', 'album');
+      expect(httpClientStub.put).toHaveBeenCalledWith('https://api.spotify.com/v1/me/player/play', {context_uri: 'randomuri'});
+    });
+  });
 
 
 
