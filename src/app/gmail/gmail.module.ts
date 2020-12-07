9ea2ffb78209c67f39940ac629d36ee9199deb14
GuiBret <guillaume.bretzner@gmail.com>
2020-12-07 20:10:38 +0100

    Add "Read an email" component

--- src/app/gmail/gmail.module.ts
+++ src/app/gmail/gmail.module.ts
@@ -4,13 +4,15 @@ import { GmailRoutingModule } from './gmail.routing';
 import { GmailStoreTokenComponent } from './components/gmailstoretoken/gmailstoretoken.component';
 import { GmailEmailListComponent } from './components/gmail-email-list/gmail-email-list.component';
 import { SharedModule } from '../shared/shared.module';
+import { ReadEmailComponent } from './pages/read-email/read-email.component';
 
 
 @NgModule({
   declarations: [
     GmailComponent,
     GmailStoreTokenComponent,
-    GmailEmailListComponent
+    GmailEmailListComponent,
+    ReadEmailComponent
   ],
   imports: [
     GmailRoutingModule,
--- src/app/gmail/gmail.routing.ts
+++ src/app/gmail/gmail.routing.ts
@@ -2,11 +2,15 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from '@angular/router';
 import { GmailComponent } from './gmail.component';
 import { GmailStoreTokenComponent } from './components/gmailstoretoken/gmailstoretoken.component';
+import { ReadEmailComponent } from './pages/read-email/read-email.component';
 
 
 const routes: Routes = [
   {
     path: 'gmail', component: GmailComponent
+  },
+  {
+    path: 'gmail/read-email/:emailid', component: ReadEmailComponent
   },
   {
     path: 'gmail/store-token/:token/:expires', component: GmailStoreTokenComponent
