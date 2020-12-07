dd4b6a6db1846672d6ee6fde0fa8e310b7d8ea44
GuiBret <guillaume.bretzner@gmail.com>
2020-12-07 21:04:43 +0100

    Add redirect when logged

--- src/app/gmail/components/gmailstoretoken/gmailstoretoken.component.ts
+++ src/app/gmail/components/gmailstoretoken/gmailstoretoken.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-gmailstoretoken',
@@ -8,13 +8,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class GmailStoreTokenComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
 
     this.route.params.subscribe((params: any) => {
       localStorage.setItem('gmailToken', params.token);
       localStorage.setItem('gmailExp', params.expires);
+
+      this.router.navigate(['gmail']);
     });
   }
 
