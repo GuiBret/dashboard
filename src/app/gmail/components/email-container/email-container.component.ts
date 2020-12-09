449e387991bde0be8fe9f4a5ee74e12b3cfb4076
GuiBret <guillaume.bretzner@gmail.com>
2020-12-10 00:08:53 +0100

    Fix overlapping styles

--- src/app/gmail/components/email-container/email-container.component.ts
+++ src/app/gmail/components/email-container/email-container.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-email-container',
   templateUrl: './email-container.component.html',
-  styleUrls: ['./email-container.component.css']
+  styleUrls: ['./email-container.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom // Used to contain the styles inside the component
 })
 export class EmailContainerComponent implements OnInit {
 
+  @Input() htmlContent: string;
   constructor() { }
 
   ngOnInit(): void {
