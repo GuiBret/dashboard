02c1e0bb15268204e67b2461fa45addee06df0f0
GuiBret <guillaume.bretzner@gmail.com>
2020-12-19 23:33:21 +0100

    Update docs

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
+/**
+ * Component representing the home page of the dashboard, describing its features
+ */
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent {
 
+  /**
+   * The constructor
+   * @param titleSvc The title service, to change the window title
+   */
   constructor(private titleSvc: Title) {
     this.titleSvc.setTitle('Dashboard - Home Page')
   }
-
-  ngOnInit(): void {
-  }
-
 }
