33328372e988e1025a87298b21056255b1bf74ae
GuiBret <guillaume.bretzner@gmail.com>
2021-01-03 22:59:37 +0100

    Add test

--- src/app/gmail/services/gmail.service.spec.ts
+++ src/app/gmail/services/gmail.service.spec.ts
@@ -7,7 +7,8 @@ import { GmailService } from './gmail.service';
 describe('GmailService', () => {
   let service: GmailService;
   const httpServiceStub: Partial<HttpService> = {
-    modifyEmail: () => new Observable()
+    modifyEmail: () => new Observable(),
+    getIndividualEmailInfo: jasmine.createSpy().and.returnValue(new Observable())
   };
 
   beforeEach(() => {
@@ -21,6 +22,62 @@ describe('GmailService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('On email list received', () => {
+    it('should have posted an empty array and stored an empty array since we have no results', () => {
+
+      // tslint:disable-next-line: no-string-literal
+      spyOn(service['newEmailListPosted'], 'next');
+
+      const mockEmptyResponse = {
+        messages: [],
+        resultSizeEstimate : 0,
+        nextPageToken: 'abc'
+      };
+
+      // tslint:disable-next-line: no-string-literal
+      service['messageBox'] = [{}, {}, {}];
+
+      service.onEmailListReceived('init', mockEmptyResponse);
+
+      // tslint:disable-next-line: no-string-literal
+      expect(service['messageBox'].length).toEqual(0);
+      // tslint:disable-next-line: no-string-literal
+      expect(service['newEmailListPosted'].next).toHaveBeenCalledWith([]);
+
+    });
+
+    it('should have added a new token since we are at the last index', () => {
+
+
+      spyOn<any>(service, 'makeGetCallOnEmail');
+
+      // tslint:disable-next-line: no-string-literal
+      service['tokens'] = [null, 'a', 'b', 'c'];
+      // tslint:disable-next-line: no-string-literal
+      service['currTokenIdx'] = 3;
+
+      const mockResponse = {
+        messages: [
+          {id: '1', threadId: 'a'},
+          {id: '2', threadId: 'a'},
+          { id: '3', threadId: 'a' },
+          { id: '4', threadId: 'a' },
+          { id: '5', threadId: 'a' }
+        ],
+        resultSizeEstimate : 5,
+        nextPageToken: 'd'
+      };
+
+
+      service.onEmailListReceived('next', mockResponse);
+
+      // tslint:disable-next-line: no-string-literal
+      expect(service['tokens'].length).toEqual(5);
+      // tslint:disable-next-line: no-string-literal
+      expect(service['makeGetCallOnEmail']).toHaveBeenCalledTimes(5);
+    });
   });
 
   describe('Toggle important email', () => {
@@ -47,7 +104,44 @@ describe('GmailService', () => {
       });
 
     });
+
+    it('should remove the IMPORTANT label since it is marked as such', () => {
+      const mockEmail = {
+        id: 'a',
+        snippet: 'abc',
+        isRead: true,
+        selected: false,
+        from: 'element@truc.com',
+        internalDate: 123456,
+        subject: 'Subject',
+        htmlContent: '<p></p>',
+        important: true
+      };
+
+      spyOn(httpServiceStub, 'modifyEmail');
+
+      service.toggleImportantEmail(mockEmail);
+
+      expect(httpServiceStub.modifyEmail).toHaveBeenCalledWith(mockEmail.id, {
+        removeLabelIds: ['IMPORTANT']
+      });
+
+    });
   });
 
+  describe('Reset tokens', () => {
+    it('should have reset the tokens array and the token ID', () => {
+      // tslint:disable-next-line: no-string-literal
+      service['tokens'] = [null, 'a', 'b', 'c'];
+      // tslint:disable-next-line: no-string-literal
+      service['currTokenIdx'] = 2;
 
+      service.resetTokens();
+
+      // tslint:disable-next-line: no-string-literal
+      expect(service['tokens'].length).toEqual(1);
+      // tslint:disable-next-line: no-string-literal
+      expect(service['currTokenIdx']).toEqual(0);
+    });
+  });
 });
