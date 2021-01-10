3d6ae577c09fd3a1bff7c6027b693dc0860bba45
GuiBret <guillaume.bretzner@gmail.com>
2021-01-10 11:01:28 +0100

    Rewrite Gmail email interface

--- src/app/gmail/interfaces/gmail-email.interface.ts
+++ src/app/gmail/interfaces/gmail-email.interface.ts
@@ -1,26 +1,37 @@
 export class GmailEmail {
   id: string;
-  internalDate: number;
-  labelIds?: Array<string>;
-  payload: {
-    headers: [
-      {
-        name:string,
-        value: string
-      }
-    ],
-    body: {
-      size: number,
-      data: string
-    },
-    parts: [
-      {
-        mimeType: string,
-        body: {
-          data: string
-        }
-      }
-    ]
-  };
+  threadId: string;
+  labelIds: string[];
   snippet: string;
+  payload: Payload;
+  sizeEstimate: number;
+  historyId: string;
+  internalDate: string;
+}
+
+interface Payload {
+  partId: string;
+  mimeType: string;
+  filename: string;
+  headers: Header[];
+  body: Body;
+  parts: GmailEmailPart[];
+}
+
+export class GmailEmailPart {
+  partId: string;
+  mimeType: string;
+  filename: string;
+  headers: Header[];
+  body: Body;
+}
+
+interface Body {
+  size: number;
+  data?: string;
+}
+
+interface Header {
+  name: string;
+  value: string;
 }
