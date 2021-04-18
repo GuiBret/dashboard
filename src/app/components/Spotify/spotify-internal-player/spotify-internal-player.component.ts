7ea3d65000bb07afbb445405f5b82a5cac1d399c
GuiBret <guillaume.bretzner@gmail.com>
2021-04-18 18:19:22 +0200

    Add comments

--- src/app/components/Spotify/spotify-internal-player/spotify-internal-player.component.ts
+++ src/app/components/Spotify/spotify-internal-player/spotify-internal-player.component.ts
@@ -2,7 +2,6 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { SpotifyPlayerService } from 'src/app/Spotify/services/spotify-player.service';
 import { SpotifyService } from 'src/app/Spotify/services/spotify.service';
 
-
 @Component({
   selector: 'app-spotify-internal-player',
   templateUrl: './spotify-internal-player.component.html',
@@ -19,32 +18,30 @@ export class SpotifyInternalPlayerComponent implements OnInit {
 
     window.onSpotifyWebPlaybackSDKReady = this.onSpotifyWebPlaybackSDKReady.bind(this);
 
-
-
   }
   private onSpotifyWebPlaybackSDKReady() {
     // We connect to the Playback SDK only if we are connected to Spotify
     if(this.spotifySvc.checkSpotifyStatus()) {
       const token = localStorage.getItem('spotifyToken');
-      this.player = new Spotify.Player({
+      this.player = new window.Spotify.Player({
         name: 'Dashboard Player',
         getOAuthToken: cb => { cb(token); }
       });
 
-      // // Error handling
-      // TODO: handle errors
+
+      // TODO: improve error handling
       this.player.addListener('initialization_error', this.triggerErrorMessage.bind(this, "Initialization error"));
       this.player.addListener('authentication_error', this.triggerErrorMessage.bind(this, "Authentication error"));
       this.player.addListener('account_error', this.triggerErrorMessage.bind(this, "Account error"));
       this.player.addListener('playback_error', this.triggerErrorMessage.bind(this, "Playback error"));
 
-      // // Playback status updates
+      // Playback status updates
       this.player.addListener('player_state_changed', state => { this.spotifyPlayerSvc.pushPlaybackMetadataChanged(state); });
 
-      // // Ready
+      // Ready
       this.player.addListener('ready', this.onPlayerReady.bind(this));
 
-      // // Connect to the player!
+      // Connect to the player!
       this.player.connect().then(this.onPlayerConnected.bind(this));
 
     }
@@ -59,6 +56,8 @@ export class SpotifyInternalPlayerComponent implements OnInit {
   }
 
   private onPlayerConnected() {
+
+    // Change position in song
     this.spotifyPlayerSvc.onSeekPositionRequested.subscribe(position => {
       this.player.seek(position);
     })
