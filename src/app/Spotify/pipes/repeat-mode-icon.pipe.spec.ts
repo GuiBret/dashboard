bdfb192af7513bab2b9a8601fa98e5c01ea3f383
GuiBret <guillaume.bretzner@gmail.com>
2021-04-27 19:30:25 +0200

    Add tests

--- src/app/Spotify/pipes/repeat-mode-icon.pipe.spec.ts
+++ src/app/Spotify/pipes/repeat-mode-icon.pipe.spec.ts
@@ -1,8 +1,39 @@
 import { RepeatModeIconPipe } from './repeat-mode-icon.pipe';
 
 describe('RepeatModeIconPipe', () => {
-  it('create an instance', () => {
+  it('should create an instance', () => {
     const pipe = new RepeatModeIconPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return "repeat" when providing an invalid value', () => {
+    const pipe = new RepeatModeIconPipe();
+
+    const value = pipe.transform('abcdef');
+
+    expect(value).toEqual('repeat');
+  });
+  it('should return "repeat" when providing "off"', () => {
+    const pipe = new RepeatModeIconPipe();
+
+    const value = pipe.transform('off');
+
+    expect(value).toEqual('repeat');
+  });
+
+  it('should return "repeat_one" when providing "track"', () => {
+    const pipe = new RepeatModeIconPipe();
+
+    const value = pipe.transform('track');
+
+    expect(value).toEqual('repeat_one');
+  });
+
+  it('should return "repeat_on" when providing "context"', () => {
+    const pipe = new RepeatModeIconPipe();
+
+    const value = pipe.transform('context');
+
+    expect(value).toEqual('repeat_on');
   });
 });
